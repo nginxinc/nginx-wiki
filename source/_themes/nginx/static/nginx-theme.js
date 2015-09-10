be67f58fde4ab8b0b6b254dcf94a10015f89d07a
Andrew Hutchings <andrew@linuxjedi.co.uk>
2015-09-10 15:39:13 +0100

    Make sticky header the only header
    Remove unneeded stuff from the header too, fixup the CSS

--- source/_themes/nginx/static/nginx-theme.js
+++ source/_themes/nginx/static/nginx-theme.js
@@ -83,47 +83,6 @@
     };
 
     $(document).ready(function() {
-        // FREE TRIAL MODAL JS
-
-        $('.free-trial-inner-wrap').click(function(e) {
-            if(e.target == this) {
-                nginxTheme.toggleFreeTrial();
-            }
-        });
-
-        $('.free-trial-wrap .close-button').click(nginxTheme.toggleFreeTrial);
-        $('a[href="#free-trial"]').click(function(e) {
-            e.preventDefault();
-            nginxTheme.toggleFreeTrial();
-        });
-
-        if(window.location.hash == "#free-trial") {
-            nginxTheme.toggleFreeTrial();
-        }
-
-        if(window.location.hash == "#hack") {
-            nginxTheme.toggleFreeTrial();
-        }
-
-
-        // CONTACT US MODAL JS
-        $('.contact-us-inner-wrap').click(function(e) {
-            if(e.target == this) {
-                nginxTheme.toggleContactUs();
-            }
-        });
-
-        $('.contact-us-wrap .close-button').click(nginxTheme.toggleContactUs);
-        $('a[href="#contact-us"]').click(function(e) {
-            e.preventDefault();
-            nginxTheme.toggleContactUs();
-        });
-
-
-        if(window.location.hash == "#contact-us") {
-            nginxTheme.toggleContactUs();
-        }
-
 
         // Toggle mobile header.
         $('.mobile-menu-button').on('click', function(e) {
@@ -137,23 +96,6 @@
             nginxTheme.mobilePrimaryMenuItemToggle(this);
         });
 
-        // Toggle menu search
-        $('.search-field')
-            .on('focus', function() {
-                $(this).closest('.search-form-wrapper').addClass('focused');
-                if(!$("#masthead").hasClass("sticky-menu")) {
-                    $('.header-extras-wrapper').not('.mobile-expanded .header-extras-wrapper').toggle();
-                }
-            })
-            .on('blur', function() {
-                $(this).closest('.search-form-wrapper').removeClass('focused');
-                if(!$("#masthead").hasClass("sticky-menu")) {
-                    setTimeout(function () {
-                        $('.header-extras-wrapper').not('.mobile-expanded .header-extras-wrapper').fadeToggle();
-                    }, 1000);
-                }
-            });
-
         // Apply image link modals.
         nginxTheme.applyImageModals('.entry-content');
 
@@ -162,50 +104,5 @@
         });
 
     });
-
-    scroll_effect();
-
-
-    function scroll_effect() {
-        "use strict";
-        var headerBar = $("#masthead.site-header");
-        var lastScrollTop = 0;
-        $(window).scroll(function () {
-            var currentScrollValue = $(this).scrollTop();
-            //console.log(currentScrollValue);
-            if(headerBar.hasClass('mobile-expanded')) { return; }
-
-            if (currentScrollValue > 150) {
-                headerBar.addClass('sticky-menu');
-                if($(".post").length>0 ){
-                    //if(currentScrollValue>600){
-                    //    $("#asidewrapper aside:eq(0)").hide(200);
-                    //}else if(currentScrollValue<600){
-                    //    $("#asidewrapper aside:eq(0)").show(200);
-                    //}
-                }else if($("article.nx_info_types-admin-guide").length>0){
-                    if(currentScrollValue>400){
-                        $("#asidewrapper aside:eq(1)").hide(200);
-                    }else if(currentScrollValue<400){
-                        $("#asidewrapper aside:eq(1)").show(200);
-                    }
-                }
-
-                if (currentScrollValue > 200) {
-                    headerBar.addClass('show-sticky-menu');
-                    $("#menu-header-actions .menu-item a[href$='/products/pricing/']").hide();
-                }
-            }
-            if (currentScrollValue < 200) {
-                headerBar.removeClass('show-sticky-menu');
-                $("#asidewrapper").css("paddingTop",0);
-                if (currentScrollValue < 150) {
-                    headerBar.removeClass('sticky-menu');
-                    $("#menu-header-actions .menu-item a[href$='/products/pricing/']").show();
-                }
-            }
-            lastScrollTop = currentScrollValue;
-        });
-    }
 
 })(jQuery);
