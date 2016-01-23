43996999fd8f5376bb7fedd1f5d81339ea88dc0f
Andrew Hutchings <andrew@linuxjedi.co.uk>
2016-01-23 06:59:40 +0000

    Remove JS opening links in new tab
    Reverting #148 due to conflict with upcoming code

--- source/_themes/nginx/static/nginx-theme.js
+++ source/_themes/nginx/static/nginx-theme.js
@@ -105,10 +105,4 @@
 
     });
 
-    $('a.reference.external').each(function() {
-        $(this).click(function(event) {
-            event.preventDefault(); event.stopPropagation();window.open(this.href, '_blank');
-        });
-    });
-
 })(jQuery);
