710f262f99703763abd4820e1f2f7ab44fc18078
alessfg <alessfg@hotmail.com>
2022-02-23 13:22:30 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -316,7 +316,7 @@ linkcheck_ignore = [
                     r'.*calomel.org.*', # urlopen error EOF occurred in violation of protocol (_ssl.c:600)
                     r'.*linux.die.net/*', # Network unreachable on CI
                     r'.*catap.ru.*' # 302 error
-                    r'*github*' # GitHub URLs
+                    r'.*github.*' # GitHub URLs
                    ]
 
 
